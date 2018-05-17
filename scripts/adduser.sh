743e3ef0a8fdce17151115a5ffc7e903b5b35975
Rid <grant@seednet.eu>
2018-05-17 18:09:44 +0100

    Added add/delete/mod user

---
+++ scripts/adduser.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+EXPECTED_ARGS=4
+E_BADARGS=65
+
+if [ $# -ne $EXPECTED_ARGS ]
+    then
+    echo "Usage: `basename $0` <username> <password> <restricted folder> <read only>"
+    exit $E_BADARGS
+fi
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+USERNAME=${1}
+PASSWORD=${2}
+RESTRICTED_FOLDER=${3}
+READ_ONLY=${4}
+
+if [[ ${READ_ONLY} == '1' ]]
+    then
+    (echo ${PASSWORD}; echo ${PASSWORD}) | pure-pw useradd ${USERNAME} -f /etc/pure-ftpd/passwd/pureftpd.passwd -m -d /home/ftpusers/default/${RESTRICTED_FOLDER} -u 1001 -g 1001
+else
+    (echo ${PASSWORD}; echo ${PASSWORD}) | pure-pw useradd ${USERNAME} -f /etc/pure-ftpd/passwd/pureftpd.passwd -m -d /home/ftpusers/default/${RESTRICTED_FOLDER} -u 1000 -g 1000
+fi
---
+++ scripts/deluser.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+EXPECTED_ARGS=1
+E_BADARGS=65
+
+if [ $# -ne $EXPECTED_ARGS ]
+    then
+    echo "Usage: `basename $0` <username>"
+    exit $E_BADARGS
+fi
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+USERNAME=${1}
+
+pure-pw userdel ${USERNAME}

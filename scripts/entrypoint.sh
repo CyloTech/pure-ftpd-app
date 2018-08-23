eae21bbf0d51a3c8ca45dfb5d2c40dd8aca59222
Rid <grant@seednet.eu>
2018-08-23 15:08:09 +0100

    Always rebuild SSL

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -x
 
+cat /ssl/key.pem > /etc/ssl/private/pure-ftpd.pem
+echo '' >> /etc/ssl/private/pure-ftpd.pem
+cat /ssl/cert.pem >> /etc/ssl/private/pure-ftpd.pem
+echo '' >> /etc/ssl/private/pure-ftpd.pem
+chmod 600 /etc/ssl/private/pure-ftpd.pem
+
 if [ ! -f /etc/app_configured ]; then
     pure-pw list | grep ${FTP_USER} || (echo ${FTP_PASSWORD}; echo ${FTP_PASSWORD}) | pure-pw useradd ${FTP_USER} -f /etc/pure-ftpd/passwd/pureftpd.passwd -m -d /home/ftpusers/default -u 1000 -g 1000
-
-    cat /ssl/key.pem > /etc/ssl/private/pure-ftpd.pem
-    echo '' >> /etc/ssl/private/pure-ftpd.pem
-    cat /ssl/cert.pem >> /etc/ssl/private/pure-ftpd.pem
-    echo '' >> /etc/ssl/private/pure-ftpd.pem
-    chmod 600 /etc/ssl/private/pure-ftpd.pem
     touch /etc/app_configured
     curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST "https://api.cylo.io/v1/apps/installed/$INSTANCE_ID"
 fi
